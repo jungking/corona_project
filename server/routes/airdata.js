f04039652dc2097a5b8c354969e23cc9b6da59c0
jungking <immenige2@gmail.com>
2021-01-26 16:35:51 +0900

    fix : state name => day

--- server/routes/airdata.js
+++ server/routes/airdata.js
@@ -3,10 +3,10 @@ const serviceKey = '8pi%2BdKWKiqEdnVIk%2FGQHka7Wc7cwcZjpP18pKjlYcrtVhSPtbvfBCeg2
 //공공데이터 포털에서 받은 내 서비스키 
 
 var parse = require('json-parse');
-const airdata = (sidoName, callback) => {       // index.js에서 보내준 시/도 이름을 여기서 받았다.
+const airdata = (day, callback) => {       // index.js에서 보내준 시/도 이름을 여기서 받았다.
 
     console.log("에어데이타2!!!!!");            //진입햇는지 확인용
-
+    
     const url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?';
     //api사용하기 위한 url이다. 끝에 '?'물음표를 붙여야된다.
 
@@ -14,8 +14,8 @@ const airdata = (sidoName, callback) => {       // index.js에서 보내준 시/
     var queryParams = encodeURIComponent('serviceKey') + '=' + serviceKey   //서비스키
     queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); //페이지 번호
     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10');   //한 페이지 경과 수
-    queryParams += '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent('20200410');    //데이터 생성일 시작범위
-    queryParams += '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent('20200410');  //데이터 생성일 종료범위
+    queryParams += '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent(day);    //데이터 생성일 시작범위
+    queryParams += '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent(day);  //20210126데이터 생성일 종료범위
     //queryParams += '&' + encodeURIComponent('_returnType') + '=' + encodeURIComponent('json')   //josn으로 받기 
     
     request(
@@ -24,11 +24,11 @@ const airdata = (sidoName, callback) => {       // index.js에서 보내준 시/
         method: 'GET'
         }, function (error, response, body) 
         {
-        //console.log(url+queryParams);
+        console.log(url+queryParams);
         //console.log('Status', response.statusCode);
         //console.log('Headers', JSON.stringify(response.headers));
         
-        console.log('Reponse received', body);
+        //console.log('Reponse received', body);
 
         callback(undefined,{    //body를 air이름으로 만들어서 index.js에 보내준다
             air:body
