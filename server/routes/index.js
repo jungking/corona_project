066b71b8a7c2c2663ebbcc9403bde6e4a1cc5c8f
jungking <immenige2@gmail.com>
2021-01-26 16:35:36 +0900

    fix : server error, server connect check, state name => day

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,9 +11,9 @@ router.use(bodyParser.urlencoded({extended: true}))
 router.post('/location',function (req,res){ ///프론트에서 fetch로 요청한 location 친구
 
     console.log("연결 1!!!")    //확인용
-    airdata(req.body.sidoName,(error, {air}={})=>{  //airdata함수에 fetch해준 req->body->sidoName를 보내준다
+    airdata(req.body.day,(error, {air}={})=>{  //airdata함수에 fetch해준 req->body->day를 보내준다
         if(error){      //에러 발생시
-            console.log("가나다라마바사!!");
+            console.log("server, index.js error");
             return res.send({error})
         }
         return res.send(air);   // airdata에서 받은 객체를 프론틀앤드로 보내준다. 
