8b0ea3a423fd5f2534d2a9ba109c665fd24678cb
jungking <immenige2@gmail.com>
2021-02-04 15:29:54 +0900

    fix: fix moment time

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -3,15 +3,14 @@ import moment from 'moment';
 
 
 //var m = moment(); // m 포멧
-var m1 = moment().format('YYYYMMDD');  //12시 지나면 당연히 에러나지.. 값이 없는데
-var m2 = moment().subtract(1,'d').format('YYYYMMDD');
-var m3 = moment().subtract(2,'d').format('YYYYMMDD');
-var m4 = moment().subtract(3,'d').format('YYYYMMDD');
-var m5 = moment().subtract(4,'d').format('YYYYMMDD');
-var m6 = moment().subtract(5,'d').format('YYYYMMDD');
-var m7 = moment().subtract(6,'d').format('YYYYMMDD');
+var m1 = moment().format('YYYYMMDD');  var m11 = moment().format('YYYY-MM-DD');//12시 지나면 당연히 에러나지.. 값이 없는데
+var m2 = moment().subtract(1,'d').format('YYYYMMDD');   var m22 = moment().subtract(1,'d').format('YYYY-MM-DD'); 
+var m3 = moment().subtract(2,'d').format('YYYYMMDD');   var m33 = moment().subtract(2,'d').format('YYYY-MM-DD'); 
+var m4 = moment().subtract(3,'d').format('YYYYMMDD');   var m44 = moment().subtract(3,'d').format('YYYY-MM-DD'); 
+var m5 = moment().subtract(4,'d').format('YYYYMMDD');   var m55 = moment().subtract(4,'d').format('YYYY-MM-DD'); 
+var m6 = moment().subtract(5,'d').format('YYYYMMDD');   var m66 = moment().subtract(5,'d').format('YYYY-MM-DD'); 
+var m7 = moment().subtract(6,'d').format('YYYYMMDD');   var m77 = moment().subtract(6,'d').format('YYYY-MM-DD'); 
 
-console.log(m1,m2,m3,m4,m5,m6,m7)
 //var m1 = '20210203'
 var momen = moment().format('YYYY-MM-DD');
 /* import './main.css'; */
@@ -94,21 +93,27 @@ class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <h1>CORONA PROJECT</h1>
+                <h1 className = "title">CORONA PROJECT</h1>
                 
                 <form>
-                    <br/>
+                    
                     <button className = "btn-1" onClick = {this.search}> 최초 실행 시 클릭하여 DB에 정보 저장</button>
-                    <br/>
+                    
                     <h1>저장된 정보의 날짜는 ({momen})</h1>
                     <br/>
 
                     <h1>지역 : </h1>
                     <h4>제주, 경남, 경북, 전남, 전북, 충남, 충북, 강원 ,경기 <br/> 세종, 울산, 대전 광주, 인천, 대구, 부산, 서울, 전국</h4>
-                    
-                    <input placeholder={this.state.city} name='city'  onChange={this.onChange} />  
-                    <button className = "btn-2" onClick={this.onclick}>Search</button>  <br/>
-                    <h1>{this.state.city}의 {momen} 확진자 수 는 : {this.state.data}</h1>                 </form>
+                    <br/>
+                    <input className="textbox" placeholder={this.state.city} name='city'  onChange={this.onChange} />  
+                    <button className = "btn-2" onClick={this.onclick}>Search</button> 
+                    <h2>{this.state.city}의 {m11} 확진자 수 는 : {this.state.data}</h2>
+                    <h2>{this.state.city}의 {m22} 확진자 수 는 : {this.state.data}</h2>
+                    <h2>{this.state.city}의 {m33} 확진자 수 는 : {this.state.data}</h2>
+                    <h2>{this.state.city}의 {m44} 확진자 수 는 : {this.state.data}</h2>
+                    <h2>{this.state.city}의 {m55} 확진자 수 는 : {this.state.data}</h2>
+                    <h2>{this.state.city}의 {m66} 확진자 수 는 : {this.state.data}</h2>
+                    <h2>{this.state.city}의 {m77} 확진자 수 는 : {this.state.data}</h2>                </form>
             </div>
             
         );
