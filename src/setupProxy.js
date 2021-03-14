27ef6888da1b309eae6fbea3d683f3cf76682f15
jungking <immenige2@gmail.com>
2021-03-15 01:36:13 +0900

    delete : proxy

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-    app.use(proxy('/api/**', { target: 'https://hjlimweb.herokuapp.com' }));
-    app.use(proxy('/otherApi/**', { target: 'https://hjlimweb.herokuapp.com' }));
-};
