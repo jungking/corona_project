bb2500fb07e94e3cb31d9efbd6fa7cca8dbe3211
jungking <immenige2@gmail.com>
2021-03-16 13:39:11 +0900

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/","location","/calldb"], { target: "http://localhost:5000" })
+    proxy(["location","/calldb"], { target: "https://hjlimweb.herokuapp.com/" })
   );
 };
