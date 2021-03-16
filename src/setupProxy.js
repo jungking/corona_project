c9225447c899641d0d4a5537409e913beadcb4a1
jungking <immenige2@gmail.com>
2021-03-16 15:43:28 +0900

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/location","/calldb",
-    createProxyMiddleware({
+    createProxyMiddleware("/location","/calldb",{
       target: "http://localhost:5000",
       changeOrigin: true,
     })
