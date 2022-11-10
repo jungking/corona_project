d41cd12317c799966c4bd2773fd2225e7c9d547a
jungking <immenige2@gmail.com>
2022-11-10 21:51:15 +0900

    f

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const cors = require('cors');                   //cors를 사용하고 싶다
 const bodyParser = require('body-parser');      //body-parser을 사용하고 싶다
 const port = process.env.PORT || 5000;                              //서버포트는 heroku 포트 또는5000번 포트
 const route = require('./routes/index');        //router가 있는 곳이다 만들 index.js다
+const path = require("path")
 app.use(bodyParser.json());
 
 app.use(cors());
 app.use('/', route);
-app.use(express.static(path.join(__dirname,"../buildd")))
+app.use(express.static(path.join(__dirname,"../build")))
 /*
 app.use(express.static(path.join(__dirname, '../build')));
 
